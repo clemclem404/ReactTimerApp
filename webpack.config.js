bbb7c0c00ec698499192c54e54ae5683e308ad6e
paulinerousseau <pauline_rousseau@hotmail.co.uk>
2016-11-01 16:02:54 -0400

    clock component

--- webpack.config.js
+++ webpack.config.js
@@ -30,8 +30,8 @@ plugins: [
       applicationStyles: 'app/styles/app.scss',
       Navigation: 'app/components/Navigation.jsx',
       Timer: 'app/components/Timer.jsx',
-      Countdown: 'app/components/Countdown.jsx'
-
+      Countdown: 'app/components/Countdown.jsx',
+      Clock: 'app/components/Clock.jsx'
 
     },
 
